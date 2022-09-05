939d8398526bebe2b04eb331514d4c9eac56ce9e
galactaporte <sharifah3799@gmail.com>
2022-09-05 13:42:26 +0800

    file upload database3

--- routes/books.js
+++ routes/books.js
@@ -111,7 +111,7 @@ function saveCover(book, coverEncoded) {
     if(coverEncoded ==nul) return
     const cover = JSON.parse(coverEncoded)
     if (cover !=null && imageMimeTypes.includes(cover.type)) {
-        book.coverImage = new Buffer.from(cover.data, "base64")
+        book.coverImage = new Buffer.from(cover.data, 'base64')
         book.coverImageType = cover.type
     }
 }
